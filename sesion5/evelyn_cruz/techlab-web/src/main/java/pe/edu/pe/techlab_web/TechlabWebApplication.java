ae3affedfc5fd027665911e0e47496029dbe41bf
Evelyn <evidendz@gmail.com>
2026-09-09 19:49:00 -0500

    spring xd

---
+++ sesion5/evelyn_cruz/techlab-web/src/main/java/pe/edu/pe/techlab_web/TechlabWebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.techlab_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlabWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechlabWebApplication.class, args);
+	}
+
+}
