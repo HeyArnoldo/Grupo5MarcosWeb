5e01ea1c67b369dbecef2aa8a701524b518146f3
Evelyn <evidendz@gmail.com>
2026-09-23 19:57:42 -0500

    xd

---
+++ Laboratorio6/evelyn_cruz/techlab-web/src/test/java/pe/edu/utp/techlab_web/TechlabWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.techlab_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechlabWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
